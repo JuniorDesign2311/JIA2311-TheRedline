26745194f0f04b0506584124e02fefc20b5eda7f
mjdemars <maddalendemars@gmail.com>
2023-01-20 17:18:33 -0500

    Importing packages for bottom sheet modal

--- AIP App/screens/MapScreen.js
+++ AIP App/screens/MapScreen.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import * as React from 'react';
 import MapView from 'react-native-maps';
 import { PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import Animated from 'react-native-reanimated';
+import BottomSheet from 'reanimated-bottom-sheet';
 
 export default function MapScreen() {
   return (

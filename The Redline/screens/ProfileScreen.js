d3033209badbcf531e9a0945f4869e08d3e0c26e
jshkm <2joshmail@gmail.com>
2023-02-27 18:17:28 -0500

    added username to profile screen

--- The Redline/screens/ProfileScreen.js
+++ The Redline/screens/ProfileScreen.js
@@ -1,10 +1,32 @@
-import React, {useState, useRef, useMemo} from 'react'
+import React, {useState, useEffect, useMemo} from 'react'
 import { View, Text,  StyleSheet, TouchableOpacity} from 'react-native'
 import CustomButton from '../components/CustomButton';
 import KeyboardAvoidingWrapper from '../components/KeyboardAvoidingWrapper';
 import GlobalStyles from '../components/GlobalStyles';
+import firebase from "firebase/app";
 
 const ProfileScreen = ({navigation, route}) => {
+    const [username, setUsername] = useState('');
+
+    const user = firebase.auth().currentUser;
+
+    useEffect(() => {
+        firebase.firestore().collection("hosts").doc(user.uid).get().then((snapshot) => {
+            console.log(snapshot.data());
+            if (snapshot.exists) {
+                const userData = snapshot.data();
+                setUsername(userData["username"].toString())
+            } else {
+                firebase.firestore().collection("attendees").doc(user.uid).get().then((snapshot) => {
+                    if (snapshot.exists) {
+                        const userData = snapshot.data();
+                        setUsername(userData["username"].toString())
+                    }
+                })
+            }
+        })
+    }, []);
+
     const onSettingsPressed = () => {
         navigation.navigate("Settings");
     }
@@ -16,7 +38,7 @@ const ProfileScreen = ({navigation, route}) => {
     return (
         <KeyboardAvoidingWrapper>
         <View style={GlobalStyles.viewStyle}>
-            <Text style={[GlobalStyles.header]}> Profile </Text>
+            <Text style={[GlobalStyles.header]}> {username} </Text>
             <CustomButton onPress={onSettingsPressed} buttonName="Settings" type="PRIMARY"/>
             <CustomButton onPress={onSignOutPressed} buttonName="Sign Out" type="PRIMARY"/>
         </View>

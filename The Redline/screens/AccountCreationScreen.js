07a06a23c781919746584d4e84caf79388f7c21c
nicolehu43 <122466449+nicolehu43@users.noreply.github.com>
2023-04-05 09:58:12 -0400

    removed the useStates and implemented noError

--- The Redline/screens/AccountCreationScreen.js
+++ The Redline/screens/AccountCreationScreen.js
@@ -29,12 +29,6 @@ const AccountCreationScreen = ({ navigation }) => {
     const [isValidPassword, setIsValidPassword] = useState(true);
     const [isValidConfirm, setIsValidConfirm] = useState(true);
     const [isValidPhone, setIsValidPhone] = useState(true);
-    // Variable check to navigate to next screen
-    const [usernameNavigationCheck, setUsernameNavigationCheck] = useState(false);
-    const [emailNavigationCheck, setEmailNavigationCheck] = useState(false);
-    const [passwordNavigationCheck, setPasswordNavigationCheck] = useState(false);
-    const [confirmNavigationCheck, setConfirmNavigationCheck] = useState(false);
-    const [phoneNavigationCheck, setPhoneNavigationCheck] = useState(false);
 
     // Method that validates the inputs within each of the fields
     const validateInput = () => {
@@ -42,28 +36,29 @@ const AccountCreationScreen = ({ navigation }) => {
         var hostRef = db.collection("hosts");
         var attendeeRef = db.collection("attendees");
 
+        var noError = true;
+
         if (!username) {
+            noError = false;
             setUsernameError('Username Field is Empty');
             setIsValidUsername(false);
-            setUsernameNavigationCheck(false);
         } else if (username.indexOf(' ') >= 0) {
+            noError = false;
             setUsernameError('Username Cannot Contain Spaces');
             setIsValidUsername(false);
-            setUsernameNavigationCheck(false);
         } else if (username.indexOf('&') >= 0 || username.indexOf('=') >= 0 || username.indexOf('_') >= 0 || username.indexOf("'") >= 0 || username.indexOf('-') >= 0 || username.indexOf('%') >= 0 || username.indexOf('$') >= 0
                     || username.indexOf('+') >= 0 || username.indexOf(',') >= 0 || username.indexOf('<') >= 0 || username.indexOf('>') >= 0 || username.indexOf('~') >= 0 || username.indexOf('"') >= 0 || username.indexOf('.') >= 0) {
+            noError = false;
             setUsernameError('Username Cannot Contain Special Characters');
             setIsValidUsername(false);
-            setUsernameNavigationCheck(false);
         } else {
             attendeeRef.where("usernameToLowerCase", '==', username.toLowerCase()).get()
             .then(snapshot => {
                 if (snapshot.empty) {
-                    setUsernameNavigationCheck(true);
                     setUsernameError('');
                     setIsValidUsername(true);
                 } else {
-                    setUsernameNavigationCheck(false);
+                    noError = false;
                     setUsernameError('Username already taken');
                     setIsValidUsername(false);
                 }
@@ -71,11 +66,10 @@ const AccountCreationScreen = ({ navigation }) => {
             hostRef.where("usernameToLowerCase", '==', username.toLowerCase()).get()
             .then(snapshot => {
                 if (snapshot.empty) {
-                    setUsernameNavigationCheck(true);
                     setUsernameError('');
                     setIsValidUsername(true);
                 } else {
-                    setUsernameNavigationCheck(false);
+                    noError = false;
                     setUsernameError('Username already taken');
                     setIsValidUsername(false);
                 }
@@ -84,31 +78,30 @@ const AccountCreationScreen = ({ navigation }) => {
         
         // Email Validation
         if (email.length === 0) {
+            noError = false;
             setEmailError('Email Field is Empty');
             setIsValidEmail(false);
-            setEmailNavigationCheck(false);
         } else if (!email.includes('@')) {
+            noError = false;
             setEmailError('Invalid Email Address');
             setIsValidEmail(false);
-            setEmailNavigationCheck(false);
         } else if (!email.includes('.')) {
+            noError = false;
             setEmailError('Invalid Email Address');
             setIsValidEmail(false);
-            setEmailNavigationCheck(false);
         } else if (email.indexOf(' ') >= 0 || email.indexOf('&') >= 0 || email.indexOf('=') >= 0 || email.indexOf("'") >= 0 || email.indexOf('*') >= 0 || email.indexOf('%') >= 0
         || email.indexOf('+') >= 0 || email.indexOf(',') >= 0 || email.indexOf('<') >= 0 || email.indexOf('>') >= 0 || email.indexOf('$') >= 0 || email.indexOf('"') >= 0) {
+            noError = false;
             setEmailError('Email Cannot Contain Special Characters');
             setIsValidEmail(false);
-            setEmailNavigationCheck(false);
         } else {
             attendeeRef.where("emailToLowerCase", "==", email.toLowerCase()).get()
             .then(snapshot => {
                 if (snapshot.empty) {
-                    setEmailNavigationCheck(true);
                     setEmailError('');
                     setIsValidEmail(true);
                 } else {
-                    setEmailNavigationCheck(false);
+                    noError = false;
                     setEmailError('Email Address already taken');
                     setIsValidEmail(false);
                 }
@@ -116,11 +109,10 @@ const AccountCreationScreen = ({ navigation }) => {
             hostRef.where("emailToLowerCase", "==", email.toLowerCase()).get()
             .then(snapshot => {
                 if (snapshot.empty) {
-                    setEmailNavigationCheck(true);
                     setEmailError('');
                     setIsValidEmail(true);
                 } else {
-                    setEmailNavigationCheck(false);
+                    noError = false;
                     setEmailError('Email Address already taken');
                     setIsValidEmail(false);
                 }
@@ -129,60 +121,57 @@ const AccountCreationScreen = ({ navigation }) => {
 
         // Password Validation
         if (password.length === 0) {
-            setPasswordNavigationCheck(false);
+            noError = false;
             setPasswordError('Password Field is Empty');
             setIsValidPassword(false);
         } else if (password.length < 6) {
-            setPasswordNavigationCheck(false);
+            noError = false;
             setPasswordError('Password must be at least 6 characters');
             setIsValidPassword(false);
         } else if (password.length > 40) {
-            setPasswordNavigationCheck(false);
+            noError = false;
             setPasswordError("Password can't be longer than 40 charaters");
             setIsValidPassword(false);
         } else if (password.indexOf(' ') >= 0) {
-            setPasswordNavigationCheck(false);
+            noError = false;
             setPasswordError('Password Cannot Contain Spaces');
             setIsValidPassword(false);
         } else {
-            setPasswordNavigationCheck(true);
             setPasswordError('');
             setIsValidPassword(true);
         }
 
         // Confirm Password Validation
         if (!cpassword) {
-            setConfirmNavigationCheck(false);
+            noError = false;
             setConfirmError('Please confirm password');
             setIsValidConfirm(false);
         } else if (password != cpassword) {
-            setConfirmNavigationCheck(false);
+            noError = false;
             setConfirmError('Passwords do not match');
             setIsValidConfirm(false);
         } else {
-            setConfirmNavigationCheck(true);
             setConfirmError('');
             setIsValidConfirm(true);
         }
 
         // Phone Number Validation
         if (phoneNumber.length === 0) {
-            setPhoneNavigationCheck(false);
+            noError = false;
             setPhoneError('Phone Number Field is Empty');
             setIsValidPhone(false);
         } else if (phoneNumber.length != 10) {
-            setPhoneNavigationCheck(false);
+            noError = false;
             setPhoneError('Phone Number is not valid');
             setIsValidPhone(false);
         } else {
             attendeeRef.where("phoneNumber", '==', phoneNumber).get()
             .then(snapshot => {
                 if (snapshot.empty) {
-                    setPhoneNavigationCheck(true);
                     setPhoneError('');
                     setIsValidPhone(true);
                 } else {
-                    setPhoneNavigationCheck(false);
+                    noError = false;
                     setPhoneError('Phone Number linked to an account');
                     setIsValidPhone(false);
                 }
@@ -190,17 +179,16 @@ const AccountCreationScreen = ({ navigation }) => {
             hostRef.where("phoneNumber", '==', phoneNumber).get()
             .then(snapshot => {
                 if (snapshot.empty) {
-                    setPhoneNavigationCheck(true);
                     setPhoneError('');
                     setIsValidPhone(true);
                 } else {
-                    setPhoneNavigationCheck(false);
+                    noError = false;
                     setPhoneError('Phone Number linked to an account');
                     setIsValidPhone(false);
                 }
             })
         }
-        if (phoneNavigationCheck && passwordNavigationCheck && emailNavigationCheck && confirmNavigationCheck && passwordNavigationCheck) {
+        if (noError) {
             navigation.navigate('AccountCreation2', {
                 docID: (username+phoneNumber),
                 username: username,
